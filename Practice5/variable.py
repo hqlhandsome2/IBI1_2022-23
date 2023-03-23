f49932d8e8e508f770f08470523bb0f69a7efdc5
hqlhandsome2 <127087591+hqlhandsome2@users.noreply.github.com>
2023-03-23 20:19:57 +0800

    Update variable.py

--- Practice5/variable.py
+++ Practice5/variable.py
@@ -2,10 +2,14 @@
 a = -3.19
 b = -118.24
 c = 116.39
-d = abs(a-b)#calculate distance
+# use d as the distance between a and b
+d = abs(a-b)
+# use e as the distance between a and c
 e = abs(a-c)
+# use if to judge which distance is greater
 if d > e:
     print( 'Rob travel further to Los Angeles')#Los Angeles is further than Haining
+# if d=<e, compare them again
 elif d<e: print ('Rob travel further to Haining')#Haining is further than Los Angeles
 else: print("same distance")
 #Haining is further
